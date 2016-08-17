a0d99a10a5a86ce243982abb649052666e645340
stujfiter <korey.earl@gmail.com>
2016-08-17 07:59:36 -0500

    Added a public ip and script to establish ssh connection to vm

---
+++ connect.sh
@@ -0,0 +1,1 @@
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kearl@eve-kearl.eastus.cloudapp.azure.com
--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 azure vm delete --resource-group HelloWorld --name Eve --quiet
 azure storage account delete --resource-group HelloWorld evestorage --quiet
 azure network nic delete --resource-group HelloWorld --name eve-nic --quiet
+azure network public-ip delete --resource-group HelloWorld --name evepublic --quiet
 azure network vnet subnet delete --resource-group HelloWorld --vnet-name HelloWorld-vnet --name HelloWorld-subnet --quiet
 azure network vnet delete --resource-group HelloWorld --name HelloWorld-vnet --quiet
 azure group delete --name HelloWorld --quiet
