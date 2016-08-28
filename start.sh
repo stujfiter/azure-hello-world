9b8d4e12984a2305b2e3f202ebe4712464423efb
Korey Earl <korey.earl@gmail.com>
2016-08-28 00:14:23 +0000

    Parameterized the resource group in start and stop scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-azure group create --name HelloWorld --location eastus
-azure network vnet create --resource-group HelloWorld --name HelloWorld-vnet --location eastus
-azure network vnet subnet create --resource-group HelloWorld --vnet-name HelloWorld-vnet --name HelloWorld-subnet --address-prefix '10.0.0.0/24'
-azure network nsg create --resource-group HelloWorld --location eastus --name HelloWorld-nsg
-azure network public-ip create --location eastus --resource-group HelloWorld --domain-name-label eve-kearl --name evepublic
-azure network nic create --resource-group HelloWorld --name eve-nic --location eastus --subnet-name HelloWorld-subnet --subnet-vnet-name 'HelloWorld-vnet' --public-ip-name evepublic --network-security-group-name HelloWorld-nsg
-azure storage account create --sku-name LRS --location eastus --kind Storage --resource-group HelloWorld evestorage2
-azure vm create --resource-group HelloWorld --name Eve --location 'East US' --os-type Linux --image-urn UbuntuLTS --admin-username kearl --nic-name eve-nic --storage-account-name evestorage2
-azure network nsg rule create --resource-group HelloWorld --nsg-name HelloWorld-nsg --name HelloWorldInboundSSH-rule --priority 4096 --destination-port-range 22 --protocol '*' --source-port-range '*' --source-address-prefix '*' --destination-address-prefix '*' --access Allow --direction Inbound
-azure network nsg rule create --resource-group HelloWorld --nsg-name HelloWorld-nsg --name HelloWorldInboundHttp-rule --priority 4095 --destination-port-range 80 --protocol '*' --source-port-range '*' --source-address-prefix '*' --destination-address-prefix '*' --access Allow --direction Inbound
+azure group create --name $1 --location eastus
+azure network vnet create --resource-group $1 --name $1-vnet --location eastus
+azure network vnet subnet create --resource-group $1 --vnet-name $1-vnet --name $1-subnet --address-prefix '10.0.0.0/24'
+azure network nsg create --resource-group $1 --location eastus --name $1-nsg
+azure network public-ip create --location eastus --resource-group $1 --domain-name-label eve-kearl --name evepublic
+azure network nic create --resource-group $1 --name eve-nic --location eastus --subnet-name $1-subnet --subnet-vnet-name $1-vnet --public-ip-name evepublic --network-security-group-name $1-nsg
+azure storage account create --sku-name LRS --location eastus --kind Storage --resource-group $1 evestorage2
+azure vm create --resource-group $1 --name Eve --location 'East US' --os-type Linux --image-urn UbuntuLTS --admin-username kearl --nic-name eve-nic --storage-account-name evestorage2
+azure network nsg rule create --resource-group $1 --nsg-name $1-nsg --name $1InboundSSH-rule --priority 4096 --destination-port-range 22 --protocol '*' --source-port-range '*' --source-address-prefix '*' --destination-address-prefix '*' --access Allow --direction Inbound
+azure network nsg rule create --resource-group $1 --nsg-name $1-nsg --name $1InboundHttp-rule --priority 4095 --destination-port-range 80 --protocol '*' --source-port-range '*' --source-address-prefix '*' --destination-address-prefix '*' --access Allow --direction Inbound
--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
-azure network nsg rule delete --resource-group HelloWorld --nsg-name HelloWorld-nsg --name HelloWorldInboundHttp-rule --quiet
-azure network nsg rule delete --resource-group HelloWorld --nsg-name HelloWorld-nsg --name HelloWorldInboundSSH-rule --quiet
-azure vm delete --resource-group HelloWorld --name Eve --quiet
-azure storage account delete --resource-group HelloWorld evestorage2 --quiet
-azure network nic delete --resource-group HelloWorld --name eve-nic --quiet
-azure network public-ip delete --resource-group HelloWorld --name evepublic --quiet
-azure network nsg delete --resource-group HelloWorld --name HelloWorld-nsg --quiet
-azure network vnet subnet delete --resource-group HelloWorld --vnet-name HelloWorld-vnet --name HelloWorld-subnet --quiet
-azure network vnet delete --resource-group HelloWorld --name HelloWorld-vnet --quiet
-azure group delete --name HelloWorld --quiet
+azure network nsg rule delete --resource-group $1 --nsg-name $1-nsg --name $1InboundHttp-rule --quiet
+azure network nsg rule delete --resource-group $1 --nsg-name $1-nsg --name $1InboundSSH-rule --quiet
+azure vm delete --resource-group $1 --name Eve --quiet
+azure storage account delete --resource-group $1 evestorage2 --quiet
+azure network nic delete --resource-group $1 --name eve-nic --quiet
+azure network public-ip delete --resource-group $1 --name evepublic --quiet
+azure network nsg delete --resource-group $1 --name $1-nsg --quiet
+azure network vnet subnet delete --resource-group $1 --vnet-name $1-vnet --name $1-subnet --quiet
+azure network vnet delete --resource-group $1 --name $1-vnet --quiet
+azure group delete --name $1 --quiet
